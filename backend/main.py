034087bade4d1b0de395373b9a81242621185be2
Marcus Bello <marcusthetechguru@gmail.com>
2023-08-23 03:56:45 -0700

    second commit

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 
 
+from functions.database import reset_messages, store_messages
+from functions.openai_requests import convert_audio_to_text, get_chat_response
+
+
 
 # fastAPI init app
 app = FastAPI()
@@ -36,3 +40,54 @@ app.add_middleware(
 @app.get("/health")
 async def health_check():
     return {"message": "Health Check!"}
+
+
+# Reset Conversation
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()
+    return {"response": "conversation reset"}
+
+
+
+# Post bot response
+# Note: Not playing back in browser when using post request.
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+
+    # Convert audio to text - production
+    # Save the file temporarily
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
+
+    # Decode audio
+    message_decoded = convert_audio_to_text(audio_input)
+
+    # Guard: Ensure output
+    if not message_decoded:
+        raise HTTPException(status_code=400, detail="Failed to decode audio")
+
+    # Get chat response
+    chat_response = get_chat_response(message_decoded)
+
+    # Store messages
+    store_messages(message_decoded, chat_response)
+
+    # Guard: Ensure output
+    if not chat_response:
+        raise HTTPException(status_code=400, detail="Failed chat response")
+
+    # Convert chat response to audio
+    audio_output = convert_text_to_speech(chat_response)
+
+    # Guard: Ensure output
+    if not audio_output:
+        raise HTTPException(status_code=400, detail="Failed audio output")
+
+    # Create a generator that yields chunks of data
+    def iterfile():
+        yield audio_output
+
+    # Use for Post: Return output audio
+    return StreamingResponse(iterfile(), media_type="application/octet-stream")
